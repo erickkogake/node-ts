3f5ba07af3733a60f22449212ad740aba3a51606
Erick Kogake <erick.kogake@aigroup.local>
2024-02-09 10:27:15 -0300

    chore: add the pg db

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production', 'test']).default('production'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string(),
   PORT: z.number().default(3333),
 })
